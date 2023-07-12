e22b14e3ad18b8c120fb94ba7f85e3111fa0fa8c
Konstantin Smirnov <sasharugby@yandex.ru>
2023-07-12 20:09:32 +0300

    goodDz2

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int ReadInt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int func (int a,int b =0)
+{
+ if (a>b)
+ {
+    return 0;
+ }
+
+ return a+func(a+1,b);
+}
+int a = ReadInt("введите число");
+int b = ReadInt("введите число");
+
+System.Console.WriteLine(func (a,b));
